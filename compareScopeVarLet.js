f25e9bef3ef4a4a58c8206575f39ff4bcd951a15
William Spanfelner <wil-1-am@outlook.com>
2023-09-14 18:18:40 +0300

    initial commit

---
+++ compareScopeVarLet.js
@@ -0,0 +1,9 @@
+// Compare Scopes of the var and let Keywords
+var numArray =[];
+for (var i = 0; i < 3; i++) {
+    numArray.push(i);
+}
+console.log(numArray);  // [ 0, 1, 2 ]
+console.log(i);  // 3
+
+// 
