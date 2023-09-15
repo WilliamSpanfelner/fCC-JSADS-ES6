6a88b3f1f98e0eeceb4658eae5b0f04bf86c4b87
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2023-09-15 18:43:24 +0300

    Add challenge code

--- compareScopeVarLet.js
+++ compareScopeVarLet.js
@@ -29,7 +29,7 @@ console.log(printNumTwo());  // The value 3 is assigned to i counterintuitively.
 
 // Observe the effect of the let keyword
 let printNoTwo;
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 3; i++) {  // i is not in the global scpoe here
     if (i === 2) {
         printNoTwo = function() {
             return i;
@@ -39,3 +39,19 @@ for (let i = 0; i < 3; i++) {
 console.log(printNoTwo());  // 2
 console.log(i);  // i is not defined
 
+
+/*
+Fix the code so that i declared in the if statement is separate 
+from the i declared in the first line of the function. The var 
+keyword cannot be used anywhere.
+*/
+
+function checkScope() {
+    let i = 'function scope';  // changed var to let
+    if (true) {
+        let i = 'block scope';  // added let 
+        console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+}
