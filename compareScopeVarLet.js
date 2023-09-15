5bd30b1e05a2a8661563a93fc5a64fd1962284a0
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2023-09-15 17:48:47 +0300

    Add var keyword outside for loop

--- compareScopeVarLet.js
+++ compareScopeVarLet.js
@@ -7,3 +7,11 @@ console.log(numArray);  // [ 0, 1, 2 ]
 console.log(i);  // 3
 
 // the var keyword effectively makes 'i' a global variable
+
+var numbersArray = [];
+var i;  // The var keyword here effectively makes i globally available.
+for (i = 0; i < 3; i++) {
+    numbersArray.push(i);
+}
+console.log(numbersArray); 
+console.log(i);
