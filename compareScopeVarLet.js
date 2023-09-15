298359cf27ff76f31f13633e0cf94ac42877627c
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2023-09-15 18:10:14 +0300

    Add let keyword to example

--- compareScopeVarLet.js
+++ compareScopeVarLet.js
@@ -27,3 +27,15 @@ for (var i = 0; i < 3; i++) {
 console.log(printNumTwo());  // The value 3 is assigned to i counterintuitively...
 
 
+// Observe the effect of the let keyword
+let printNoTwo;
+for (let i = 0; i < 3; i++) {
+    if (i === 2) {
+        printNoTwo = function() {
+            return i;
+        };
+    }
+}
+console.log(printNoTwo());  // 2
+console.log(i);  // i is not defined
+
