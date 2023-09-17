2b1ab0c184d95eed93f7693eeef69f733b98b05c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 20:48:05 +0300

    Challenge - start

--- 10_assigningVarsWithDestructuring.js
+++ 10_assigningVarsWithDestructuring.js
@@ -3,3 +3,17 @@ const user = { name: 'John Doe', age: 34 };
 
 // Give new names as follows:
 const { name: userName, age: userAge } = user;
+
+// Rename vars with destructuring
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+    
+  const highToday = HIGH_TEMPERATURES.today;
+  const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+  
+  // Only change code above this line
