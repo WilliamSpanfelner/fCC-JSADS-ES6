0d870c7cf609824edc1e709a6f7e788f0aaa798d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 19:44:27 +0300

    Add a function with a default parameter

--- 06_setDefaultFunctionParameters.js
+++ 06_setDefaultFunctionParameters.js
@@ -1,1 +1,6 @@
 // Set Default Parameters for Your Functions
+const greeting = (name = "Anonymous") => "Hello " + name;
+
+console.log(greeting("John"));  // Hello John
+console.log(greeting());  // Hello Anonymous
+
