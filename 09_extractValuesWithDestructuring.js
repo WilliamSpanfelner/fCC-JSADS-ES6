53f3ff20f54923544c8c9395cc113cd1a90104e7
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 20:34:21 +0300

    Add object example

--- 09_extractValuesWithDestructuring.js
+++ 09_extractValuesWithDestructuring.js
@@ -1,1 +1,11 @@
 // Use Destructuring Assignment to Extract Values from Objects
+// Destructuring assignment neatly assigns values taken
+//  directly from an object
+
+const user = {name: 'John Doe', age: 34}
+
+const name = user.name;
+const age = user.age;
+
+console.log(name);  // John Doe
+console.log(age);  // 34
