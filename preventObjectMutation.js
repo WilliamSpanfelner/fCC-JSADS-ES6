638725030a2ecacd4507d6ee1a6adae74fe6ba84
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 17:58:49 +0300

    Challenge - prevent object changing

--- preventObjectMutation.js
+++ preventObjectMutation.js
@@ -8,3 +8,23 @@ Object.freeze(obj);  // Object is 'frozen' on execution
 obj.review = "bad";
 obj.newProp = "Test";
 console.log(obj);  // { name: 'FreeCodeCamp', review: 'Awesome' }
+
+
+// Prevent constants from changing
+function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+    // Only change code above this line
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch (ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+
+const PI = freezeObj();
+console.log(PI);
