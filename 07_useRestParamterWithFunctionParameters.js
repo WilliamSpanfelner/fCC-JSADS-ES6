b69d0b672d72d9b1fab801d6aa3643bbaf4951b4
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 19:57:58 +0300

    Add example of rest parameter

--- 07_useRestParamterWithFunctionParameters.js
+++ 07_useRestParamterWithFunctionParameters.js
@@ -1,3 +1,9 @@
 // Use the Rest Parameters with Function Parameters
 // The rest parameter allow functions to take a variable 
 // number of arguments
+function howMany(...args) {
+    return "You have passed " + args.length + " arguments.";
+}
+
+console.log(howMany(0, 1, 2));  // You have passed 3 arguments.
+console.log(howMany("string", null, [1, 2, 3, 4, 5], {}));  // You have passed 4 arguments.
