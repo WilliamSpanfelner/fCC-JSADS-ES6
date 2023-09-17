747bd33c75d6a6a919933c16343b20810e6e50d8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 19:47:10 +0300

    Challenge - start

--- 06_setDefaultFunctionParameters.js
+++ 06_setDefaultFunctionParameters.js
@@ -4,3 +4,5 @@ const greeting = (name = "Anonymous") => "Hello " + name;
 console.log(greeting("John"));  // Hello John
 console.log(greeting());  // Hello Anonymous
 
+// Add default parameter to the function
+const increment = (number, value) => number + value;
