88f2265638c6637e2ab9f190632a753d344b7fad
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 18:08:13 +0300

    Add example code

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,1 +1,18 @@
 // Use Arrow Functions to Write Concise Anonymous Functions
+/*
+    Anonymous functions can be written as follows:
+    const myFunc = function() {
+        const myVar = "value";
+        return myVar;
+    }
+
+    ES6 permits the following syntax:
+    const myFunc = () => {
+        const myVar = "value";
+        return myVar;
+    }
+
+    Brevity increases when there is only a return statement 
+    and no function body:
+    const myFunc = () => "value";
+*/
