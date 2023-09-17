e24402d0cd14da58d68284761a8ead3182c0750d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 19:49:34 +0300

    Challeng - solution

--- 06_setDefaultFunctionParameters.js
+++ 06_setDefaultFunctionParameters.js
@@ -5,4 +5,7 @@ console.log(greeting("John"));  // Hello John
 console.log(greeting());  // Hello Anonymous
 
 // Add default parameter to the function
-const increment = (number, value) => number + value;
+const increment = (number, value = 1) => number + value;
+
+console.log(increment(4));  // 5
+console.log(increment(4, 2));  // 6
