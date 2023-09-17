d146fa4474343011494acc0b63a6e4d03bcb6f7c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 19:31:26 +0300

    Challenge - solution

--- arrowFunctionsWithParameters.js
+++ arrowFunctionsWithParameters.js
@@ -10,8 +10,10 @@ const multiplier = (item, multi) => item * multi;
 multiplier(4, 2);
 
 // Rewrite myConcat function to use arrow function syntax
-var myConcat = function(arr1, arr2) {
-    return arr1.concat(arr2);
-}
+// var myConcat = function(arr1, arr2) {
+//     return arr1.concat(arr2);
+// }
 
-console.log(myConcat([1, 2], [3, 4, 5]));
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));  // [ 1, 2, 3, 4, 5 ]
