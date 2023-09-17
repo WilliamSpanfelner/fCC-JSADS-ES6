4368e708192ae1fbf47c5b336963589444e35fa5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 20:06:23 +0300

    Challenge - solution

--- 07_useRestParamterWithFunctionParameters.js
+++ 07_useRestParamterWithFunctionParameters.js
@@ -9,11 +9,13 @@ console.log(howMany(0, 1, 2));  // You have passed 3 arguments.
 console.log(howMany("string", null, [1, 2, 3, 4, 5], {}));  // You have passed 4 arguments.
 
 // Modify the sum function to use the rest argument
-const sum = (x, y, z) => {
-    const args = [x, y, z];
+const sum = (...args) => {
+    // const args = [x, y, z];
     let total = 0;
     for (let i = 0; i < args.length; i++) {
         total += args[i];
     }
     return total;
 }
+
+console.log(sum(2, 2, 2, 2));  // 8
