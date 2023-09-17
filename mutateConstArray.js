d4821f50b97bf8900fd88d3f0d1ff44ba2084afd
William Spanfelner <wil-1-am@outlook.com>
2023-09-17 15:51:55 +0300

    Re-assign value to const variable

--- mutateConstArray.js
+++ mutateConstArray.js
@@ -1,2 +1,6 @@
 // the const keyword is useful for assignin values that are constants
 const s = [5, 6, 7];
+s = [1, 2, 3];  
+/* Attempting to re-assign to a const variable results 
+in a TypeError: Assignment to constant variable. */
+
