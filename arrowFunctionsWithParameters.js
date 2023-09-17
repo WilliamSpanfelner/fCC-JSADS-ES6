fcf5b6a95102a73537fdc39f1afe3b071e9bf71f
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 19:22:56 +0300

    Pass multiple parameters

--- arrowFunctionsWithParameters.js
+++ arrowFunctionsWithParameters.js
@@ -5,4 +5,6 @@ doubler(4);
 // The parentheses surrounding a single parameter are not required
 // const doubler = item => item * 2;  // the doubler assignment becomes even more breif
 
-
+// Muliple arguments may also be passed to an arrow function
+const multiplier = (item, multi) => item * multi;
+multiplier(4, 2);
