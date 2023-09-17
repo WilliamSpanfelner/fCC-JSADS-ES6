fcad431c28d848c2d9465589c717115806e67648
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 19:27:47 +0300

    Challenge - start

--- arrowFunctionsWithParameters.js
+++ arrowFunctionsWithParameters.js
@@ -8,3 +8,10 @@ doubler(4);
 // Muliple arguments may also be passed to an arrow function
 const multiplier = (item, multi) => item * multi;
 multiplier(4, 2);
+
+// Rewrite myConcat function to use arrow function syntax
+var myConcat = function(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+
+console.log(myConcat([1, 2], [3, 4, 5]));
