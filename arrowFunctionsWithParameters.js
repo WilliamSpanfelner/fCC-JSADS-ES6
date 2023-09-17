2a3470d20f6a3a0f63618f304bc18bb932e32728
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 19:17:53 +0300

    Single parameter need no ()

--- arrowFunctionsWithParameters.js
+++ arrowFunctionsWithParameters.js
@@ -1,3 +1,8 @@
 // Write Arrow Functions with Parameters
 const doubler = (item) => item * 2;
 doubler(4); 
+
+// The parentheses surrounding a single parameter are not required
+// const doubler = item => item * 2;  // the doubler assignment becomes even more breif
+
+
