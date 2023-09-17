1aa95506a9737fc06cfd2732e9143fefbca15ce1
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 20:46:03 +0300

    Initial commit

---
+++ 10_assigningVarsWithDestructuring.js
@@ -0,0 +1,5 @@
+// Use Destructuring Assignment to Assign Variables from Objects
+const user = { name: 'John Doe', age: 34 };
+
+// Give new names as follows:
+const { name: userName, age: userAge } = user;
