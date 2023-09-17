1875f823911af06297ca2030d5934a337f35f243
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-17 20:19:12 +0300

    Add spread operator example

--- 08_spreadOperator.js
+++ 08_spreadOperator.js
@@ -1,1 +1,13 @@
 // Use the Spread Operator to Evaluate Arrays In-Place
+/* The spread operator allows the expansion of 
+arrays and other expressions where multiple 
+parameters are expected*/
+
+// var arr = [6, 89, 3, 45];
+// var maximus = Math.max.apply(null, arr);  // Math.max expects comma separated vals but not an array 
+// hence the use of the apply function.
+
+// The spread operator allow us to write:
+const arr = [6, 89, 3, 45];
+const maximus = Math.max(...arr);
+console.log(maximus);  // 89
