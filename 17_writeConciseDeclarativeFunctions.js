d6eb9f412f035a3e45bbc4dd33c0aefda51ef7b5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 17:41:59 +0300

    Add an object with a function as a property value

--- 17_writeConciseDeclarativeFunctions.js
+++ 17_writeConciseDeclarativeFunctions.js
@@ -1,1 +1,9 @@
 // Write Concise Declarative Functions with ES6
+const person = {
+    name: "Taylor",
+    sayHello: function() {
+        return `Hello! My name is ${this.name}.`;
+    }
+};
+
+console.log(person.sayHello());  // Hello! My name is Taylor.
