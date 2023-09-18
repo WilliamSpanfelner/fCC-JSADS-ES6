62658b668e9cefde15c34603aa210f14e1ee09b4
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 15:07:47 +0300

    Loop over failure values

--- 15_stringsWithTemplateLiterals.js
+++ 15_stringsWithTemplateLiterals.js
@@ -19,6 +19,12 @@ const result = {
   function makeList(arr) {
     // Only change code below this line
     const failureItems = [];
+
+    // loop over failure values
+
+    for (const item of result.failure) {
+        console.log(item);
+    }
     // Only change code above this line
   
     return failureItems;
