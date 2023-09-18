0820c103c416cfca88a85f3f1ec603b75089dd4c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 12:27:34 +0300

    Challenge - start

--- 13_destructuringByRestElements.js
+++ 13_destructuringByRestElements.js
@@ -4,4 +4,12 @@ console.log(a, b);  // 1 2
 console.log(arr);  // [ 3, 4, 5, 7 ]  the remainder of the array is stored 
 // in arr due to the rest syntax - note: this must be last item in list.
 
-
+// Use a destructuring assignment with the rest syntax to emulate the 
+// behavior of Array.prototype.slice(). removeFirstTwo() should return 
+// a sub-array of the original array list with the first two elements omitted.
+function removeFirstTwo(list) {
+  return list;
+}
+  
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
