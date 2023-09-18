778086ec33a29a394c2069f3a10d18e1cf45a466
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 14:16:41 +0300

    Challenge - start

--- 14_destructuringAssignmentToPassFuncParams.js
+++ 14_destructuringAssignmentToPassFuncParams.js
@@ -13,3 +13,20 @@ const profileUpdate = ({ name, age, nationality, location }) => {
 }
 // the destructure parameters are now available for use in the function.
 
+/* 
+Use destructuring assignment within the argument to the function half 
+to send only max and min values inside the function.
+*/
+
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+  };
+  
+  // Only change code below this line
+  const half = (stats) => (stats.max + stats.min) / 2.0; 
+  // Only change code above this line
