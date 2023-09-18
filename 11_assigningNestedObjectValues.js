7abc5997fa780ece94ab1e974ef9562b30c1f2c2
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 11:11:22 +0300

    Create nested object & assign to vars of same name

--- 11_assigningNestedObjectValues.js
+++ 11_assigningNestedObjectValues.js
@@ -1,1 +1,10 @@
 // Use Destructuring Assignment to Assign Variables from Nested Objects
+const user = {
+    johnDoe: {
+        age: 34,
+        email: 'johnDoe@freeCodeCamp.com'
+    }
+};
+
+// To extract the values and assign them to variables of the same name:
+const {johnDoe: {age, email}} = user;
