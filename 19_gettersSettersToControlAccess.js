fa767755b27e21976a4f484474b679b49fdb7bee
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 19:29:59 +0300

    Challenge - start

--- 19_gettersSettersToControlAccess.js
+++ 19_gettersSettersToControlAccess.js
@@ -19,3 +19,12 @@ console.log(novel.writer);
 
 // getters and setters are important to hide implementation details.
 
+// Create a Thermostat class.
+// Only change code below this line
+
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
