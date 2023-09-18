58f2e284d5cb256f8fd26cadab135c655a42b2bb
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 17:58:51 +0300

    Initial commit

---
+++ 18_defineConstructorFunc.js
@@ -0,0 +1,7 @@
+// Use class Syntax to Define a Constructor Function
+/* 
+ES6 introduces a constructor method invoked with the new keyword.
+If the constructor method is not explicitly defined, then it is 
+implicitly defined with no arguments.
+*/
+
