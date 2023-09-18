326e4d11c56378398b45b2f7cde0365a07f3aa21
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 19:01:58 +0300

    Add explicit and implicit constructors

--- 18_defineConstructorFunc.js
+++ 18_defineConstructorFunc.js
@@ -4,4 +4,24 @@ ES6 introduces a constructor method invoked with the new keyword.
 If the constructor method is not explicitly defined, then it is 
 implicitly defined with no arguments.
 */
+// Explicit constructor
+class SpaceShuttle {
+    constructor(targetPlanet) {
+        this.targetPlanet = targetPlanet;
+    }
+    takeOff() {
+        console.log("To " + this.targetPlanet + "!");
+    }
+}
+// Implicit constructor
+class Rocket {
+    launch() {
+        console.log("To the moon!");
+    }
+}
 
+const zeus = new SpaceShuttle('Jupiter');
+zeus.takeOff();  // prints To Jupiter! in console
+
+const atlas = new Rocket();
+atlas.launch();  // prints To the moon! in console
