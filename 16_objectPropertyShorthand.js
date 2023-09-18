9d0f3ae7525f577759944f5ecc3ae269a3410cfc
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 15:35:05 +0300

    Rewrite function with shorthand

--- 16_objectPropertyShorthand.js
+++ 16_objectPropertyShorthand.js
@@ -1,6 +1,8 @@
 // Write Concise Object Literal Declarations Using Object Property Shorthand
-const getMousePosition = (x, y) => ({
-    x: x,
-    y: y
-});
+// const getMousePosition = (x, y) => ({
+//     x: x,
+//     y: y
+// });
 
+// ES6 allows the same function elimating the redundancy
+const getMousePosition = (x, y) => ({ x, y });
