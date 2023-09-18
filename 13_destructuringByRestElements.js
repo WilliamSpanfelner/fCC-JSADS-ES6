2af0b71b708ba26c07846a139198fb39e8fe195d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 12:24:33 +0300

    Add destructuring example with rest syntax

--- 13_destructuringByRestElements.js
+++ 13_destructuringByRestElements.js
@@ -1,1 +1,7 @@
 // Destructuring via rest elements
+const [a, b, ...arr] = [1, 2, 3, 4, 5, 7];
+console.log(a, b);  // 1 2
+console.log(arr);  // [ 3, 4, 5, 7 ]  the remainder of the array is stored 
+// in arr due to the rest syntax - note: this must be last item in list.
+
+
