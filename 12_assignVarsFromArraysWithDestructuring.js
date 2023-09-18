da48abf4a3ff1195fa9c7d901b529f6b28fda570
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 12:13:55 +0300

    Challenge - solution

--- 12_assignVarsFromArraysWithDestructuring.js
+++ 12_assignVarsFromArraysWithDestructuring.js
@@ -2,9 +2,11 @@
 // const [a, b] = [1, 2, 3, 4, 5, 6];  // the first two elements are assigned to a and b respectively
 // console.log(a, b);  // 1 2
 
-const [a, b,,, c] = [1, 2, 3, 4, 5, 6];  // access any index using commas
-console.log(a, b, c);  // 1 2 5
+// const [a, b,,, c] = [1, 2, 3, 4, 5, 6];  // access any index using commas
+// console.log(a, b, c);  // 1 2 5
 
 // Use destructuring assignment to swap the values of a and b so that a receives
 // the value stored in b, and b receives the value stored in a.
-let aa = 8, bb = 6;
+let a = 8, b = 6;
+[a, b] = [b, a];
+console.log(a, b);  // 6 8
