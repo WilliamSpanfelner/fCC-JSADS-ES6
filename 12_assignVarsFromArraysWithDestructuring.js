a3087dae9e9cbb30e96664bde947887152a1ac96
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 11:50:18 +0300

    Access any index using multiple commas

--- 12_assignVarsFromArraysWithDestructuring.js
+++ 12_assignVarsFromArraysWithDestructuring.js
@@ -1,3 +1,6 @@
 // Use Destructuring Assignment to Assign Variables from Arrays
-const [a, b] = [1, 2, 3, 4, 5, 6];  // the first two elements are assigned to a and b respectively
-console.log(a, b);  // 1 2
+// const [a, b] = [1, 2, 3, 4, 5, 6];  // the first two elements are assigned to a and b respectively
+// console.log(a, b);  // 1 2
+
+const [a, b,,, c] = [1, 2, 3, 4, 5, 6];  // access any index using commas
+console.log(a, b, c);  // 1 2 5
