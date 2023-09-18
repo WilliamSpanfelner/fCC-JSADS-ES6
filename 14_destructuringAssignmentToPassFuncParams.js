603f1564ca22cb29fa5faabc94fc75ba26f7491b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 14:13:56 +0300

    Destructure an object in-place

--- 14_destructuringAssignmentToPassFuncParams.js
+++ 14_destructuringAssignmentToPassFuncParams.js
@@ -1,1 +1,15 @@
 // Use Destructuring Assignment to Pass an Object as a Function's Parameters
+
+// Destructuring can happen in a function argument
+
+// To destructure an object sent to a function:
+// const profileUpdate = (profileData) => {
+//     const { name, age, nationality, location } = profileData;
+// }
+
+// The above can also be done in-place:
+const profileUpdate = ({ name, age, nationality, location }) => {
+
+}
+// the destructure parameters are now available for use in the function.
+
