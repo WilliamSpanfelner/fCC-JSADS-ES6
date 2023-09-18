72fb9527f404561367d9844112e2638ae628f762
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 15:38:05 +0300

    Challenge - start

--- 16_objectPropertyShorthand.js
+++ 16_objectPropertyShorthand.js
@@ -6,3 +6,17 @@
 
 // ES6 allows the same function elimating the redundancy
 const getMousePosition = (x, y) => ({ x, y });
+
+/* 
+Use object property shorthand with object literals to create
+and return an object with name, age and gender properties.
+*/
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name: name,
+      age: age,
+      gender: gender
+    };
+    // Only change code above this line
+  };
