b2efff0a2734bb673b9526aacc49bd6720e88c7d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 20:46:56 +0300

    Challenge - solution

--- 19_gettersSettersToControlAccess.js
+++ 19_gettersSettersToControlAccess.js
@@ -21,10 +21,29 @@ console.log(novel.writer);
 
 // Create a Thermostat class.
 // Only change code below this line
-
+class Thermostat {
+    constructor(fahrenheit) {
+        this.celsius = (fahrenheit - 32) * 5 / 9;
+    }
+    get temperature() {
+        return `${this.celsius} in Celsius`;
+    }
+    set temperature(celsius) {
+        this.celsius = celsius;
+    }
+}
 // Only change code above this line
 
 const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
+console.log(temp);
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
+console.log(temp);
+
+const myThermos = new Thermostat(212);
+let myTemp = myThermos.temperature;
+console.log(myTemp);  // 100 in Celsius
+myThermos.temperature = 0;
+myTemp = myThermos.temperature;
+console.log(myTemp);  // 0 in Celsius
