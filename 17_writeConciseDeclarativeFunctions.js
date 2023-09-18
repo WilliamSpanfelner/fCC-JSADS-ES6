b37ba80dfde8625a6065b1569f52765bf7b44b25
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 17:47:30 +0300

    Rewrite sayHello property with ES6 syntax

--- 17_writeConciseDeclarativeFunctions.js
+++ 17_writeConciseDeclarativeFunctions.js
@@ -1,9 +1,16 @@
 // Write Concise Declarative Functions with ES6
+// const person = {
+//     name: "Taylor",
+//     sayHello: function() {
+//         return `Hello! My name is ${this.name}.`;
+//     }
+// };
+
+// ES6 allow the ommision of the function keyword, so person becomes:
 const person = {
     name: "Taylor",
-    sayHello: function() {
+    sayHello() {
         return `Hello! My name is ${this.name}.`;
     }
 };
-
 console.log(person.sayHello());  // Hello! My name is Taylor.
