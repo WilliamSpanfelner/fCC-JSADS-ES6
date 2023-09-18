d0f478448e52a74b25bcf317938e08c442b65c0e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 15:44:50 +0300

    Challenge - solution

--- 16_objectPropertyShorthand.js
+++ 16_objectPropertyShorthand.js
@@ -13,10 +13,6 @@ and return an object with name, age and gender properties.
 */
 const createPerson = (name, age, gender) => {
     // Only change code below this line
-    return {
-      name: name,
-      age: age,
-      gender: gender
-    };
+    return { name, age, gender };
     // Only change code above this line
   };
