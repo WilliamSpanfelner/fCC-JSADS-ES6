319790160418e841d15dc6573c075c584b2b5ff7
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 19:23:06 +0300

    Add getter/setter example

--- 19_gettersSettersToControlAccess.js
+++ 19_gettersSettersToControlAccess.js
@@ -1,1 +1,18 @@
 // Use getters adn setters to Control Access to an Object
+class Book {
+    constructor(author) {
+        this.author = author;
+    }
+    // getter
+    get writer() {
+        return this.author;
+    }
+    // setter
+    set writer(updatedAuthor) {
+        this._author = updatedAuthor;
+    }   
+}
+const novel = new Book('anonymous');
+console.log(novel.writer);
+novel.writer = 'newAuthor';
+console.log(novel.writer);
