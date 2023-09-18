81009e4606c06b0cd986a772e551970a67e15b28
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 11:27:12 +0300

    Challenge - start

--- 11_assigningNestedObjectValues.js
+++ 11_assigningNestedObjectValues.js
@@ -13,3 +13,17 @@ console.log(age, email);  // 34 johnDoe@freeCodeCamp.com
 // Assign property values to vars with different names:
 const {johnDoe: {age: userAge, email: userEmail}} = user;
 console.log(userAge, userEmail);  // 34 johnDoe@freeCodeCamp.com
+
+// Replace the two assignments with an equivalent destructuring assignment.
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Only change code below this line
+    
+  const lowToday = LOCAL_FORECAST.today.low;
+  const highToday = LOCAL_FORECAST.today.high;
+  
+  // Only change code above this line
