1a7e55dd9efe556555464fcfeed3f82d033f7117
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 14:39:08 +0300

    Challenge - solution

--- 14_destructuringAssignmentToPassFuncParams.js
+++ 14_destructuringAssignmentToPassFuncParams.js
@@ -1,17 +1,21 @@
 // Use Destructuring Assignment to Pass an Object as a Function's Parameters
 
 // Destructuring can happen in a function argument
-
+profileData = {
+    name: 'Fred',
+    age: 5,
+    nationality: 'UK',
+    location: 'Ayre',
+};
 // To destructure an object sent to a function:
 // const profileUpdate = (profileData) => {
 //     const { name, age, nationality, location } = profileData;
 // }
 
 // The above can also be done in-place:
-const profileUpdate = ({ name, age, nationality, location }) => {
-
-}
+const profileUpdate = ({ name, age, nationality, location }) => name;
 // the destructure parameters are now available for use in the function.
+console.log(profileUpdate);
 
 /* 
 Use destructuring assignment within the argument to the function half 
@@ -28,5 +32,7 @@ const stats = {
   };
   
   // Only change code below this line
-  const half = (stats) => (stats.max + stats.min) / 2.0; 
+  const half = ({ max, min }) => (max + min) / 2.0; 
   // Only change code above this line
+
+  console.log(half);
