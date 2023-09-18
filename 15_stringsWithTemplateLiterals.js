eabe6d12eb662aa5fa4ddc5565dd389c2cfb1a9a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-18 15:00:17 +0300

    Challenge - start

--- 15_stringsWithTemplateLiterals.js
+++ 15_stringsWithTemplateLiterals.js
@@ -9,3 +9,19 @@ const person = {
 const greeting = `Hello, my name is ${person.name}! 
 I am ${person.age} years old.`;
 console.log(greeting);
+
+// Create an array of list element strings.
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    // Only change code above this line
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
