d3b0d1bf62a4dbf94c93fc59f43325aaaa3e3d3c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 10:28:53 +0300

    Challeng - start

--- 22_importToReuseJS.js
+++ 22_importToReuseJS.js
@@ -4,3 +4,14 @@ import { add } from './math_functions.js';
 
 // multiple imports syntax:
 import { add, subtract } from './math_functions.js';
+
+/* Add the appropriate import statement that will allow the 
+current file to use the uppercaseString and lowercaseString
+functions you exported in the previous lesson.  These functions
+are in the file called string_functions.js, which is in the 
+same directory as the current file. */
+  
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
