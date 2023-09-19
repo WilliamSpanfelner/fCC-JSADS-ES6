317ffa9a65ab3550801168475ce6a4a3cd53b743
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 12:16:21 +0300

    Challenge - solution

--- 28_handleFulfilledPromiseWThen.js
+++ 28_handleFulfilledPromiseWThen.js
@@ -22,3 +22,7 @@ const makeServerRequest = new Promise((resolve, reject) => {
       reject("Data not received");
     }
   });
+
+  makeServerRequest.then(result => {
+    console.log(result);
+  });
