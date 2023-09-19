00a3252de487392b8dbeea72ca6091d843c70d80
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 11:23:35 +0300

    Add export default example

--- 24_exportFallback.js
+++ 24_exportFallback.js
@@ -1,1 +1,17 @@
 // Create an Export Fallback with export default
+/* Export default is used when there is only one 
+value and it is often used as a fallback. 
+Below a named function and an anonymous function 
+are exported.
+*/
+export default function add(x, y) {
+    return x + y;
+}
+
+export default function(x, y) {
+    return x + y;
+}
+
+/* Only one value can be a default export in each 
+module or file.  The keywords var, let, or const cannot
+be used with export default. */
