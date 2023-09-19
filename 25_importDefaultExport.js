661997599c7059810fa9bd6cf72edd1f55a0436d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 11:34:13 +0300

    Import a export default

--- 25_importDefaultExport.js
+++ 25_importDefaultExport.js
@@ -1,1 +1,6 @@
 // Import a Default Export
+/* A different import syntax is required to use an
+export default - no curly braces are used and the 
+imported value is simply a variable name*/
+
+import add from "./math_functions.js";
