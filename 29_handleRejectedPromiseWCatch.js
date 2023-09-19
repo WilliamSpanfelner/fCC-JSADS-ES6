0b5ab2016334ac3221561b213956125f83ef1b31
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 12:20:37 +0300

    Add catch method syntax

--- 29_handleRejectedPromiseWCatch.js
+++ 29_handleRejectedPromiseWCatch.js
@@ -1,1 +1,6 @@
 // Handle a Rejected Promise with cateh
+// When a promise is rejected the catch method is used with the following syntax:
+myPromise.catch(error => {
+
+});
+// where the error argument is passed to the reject method.
