4e30b8c18dc3006751c3d1d4793ccfa3d6d8aa59
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 10:25:51 +0300

    Add multiple import syntax

--- 22_importToReuseJS.js
+++ 22_importToReuseJS.js
@@ -1,3 +1,6 @@
 // Reuse JavaScript Code Using Import
 // import allows loading of parts of files or modules
 import { add } from './math_functions.js';
+
+// multiple imports syntax:
+import { add, subtract } from './math_functions.js';
