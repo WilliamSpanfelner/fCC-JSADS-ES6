ca49120daa6210fb6534302aeddcdeb2d01d58e8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 11:58:29 +0300

    Make promise resolve or reject

--- 27_completAPromise.js
+++ 27_completAPromise.js
@@ -1,1 +1,10 @@
 // Complete a Promise with resolve and reject
+// A promise has three states: pending, fulfilled, and rejected.
+
+const myPromise = new Promise((resolve, reject) => {
+   if (condition here) {
+    resolve("Promise was fulfilled");
+   } else {
+    reject("Promise was rejected");
+   }
+});
