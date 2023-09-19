04914b1ece0f3d603e27b765fdf95eff6e63977e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 11:51:23 +0300

    Challenge - solution

--- 26_createJSPromise.js
+++ 26_createJSPromise.js
@@ -10,3 +10,6 @@ const myPromise = new Promise((resolve, reject) => {
 
 /* Create a new promise called makeServerRequest. Pass in a 
 function with resolve and reject parameters to the constructor. */
+const makeServerRequest = new Promise((resolve, reject) => {
+    
+});
