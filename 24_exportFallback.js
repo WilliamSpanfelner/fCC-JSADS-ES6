c4e588cd20e28a27e4049508528dc51985d34cf4
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 11:27:16 +0300

    Challenge - solution

--- 24_exportFallback.js
+++ 24_exportFallback.js
@@ -19,6 +19,6 @@ be used with export default. */
 /* the following function should be the fallback value for the 
 module. Please add the necessary code to do so. */
 
-function subtract(x, y) {
+export default function subtract(x, y) {
     return x - y;
 }
