89536844c02135ad43a410d054521fb7376d7f6e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 12:13:08 +0300

    Challenge - start

--- 28_handleFulfilledPromiseWThen.js
+++ 28_handleFulfilledPromiseWThen.js
@@ -6,3 +6,19 @@ myPromise.then(result => {
 });
 
 // where result derives from the argument given to the resolve method.
+
+/*
+Add the then method to your promise. Use result as the
+parameter of its callback function and log result to 
+the console.
+*/
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
+      
+    if(responseFromServer) {
+      resolve("We got the data");
+    } else {  
+      reject("Data not received");
+    }
+  });
