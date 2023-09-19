c8d4663237afeadcfe71f09c779db90ebb587813
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 10:42:20 +0300

    Add syntax for using imported module functions

--- 23_importEverything.js
+++ 23_importEverything.js
@@ -1,3 +1,7 @@
 // Use * to Import Everything from a File
 // import * as allows the entire file to be imported
 import * as myMathModule from './math_functions.js';  // myMathModule is just a name and can be anything
+
+// The add and subtract functions discussed previously can be used like this:
+myMathModule.add(2, 3);
+myMathModule.subtract(5, 3);
