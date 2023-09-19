fc9c2eb47726d3977f72379f28137bd0b30c30ad
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 11:37:57 +0300

    Challenge - solution

--- 25_importDefaultExport.js
+++ 25_importDefaultExport.js
@@ -9,7 +9,7 @@ import add from "./math_functions.js";
 math_functions.js file, found in the same directory as this file. 
 Give the import the name subtract. */
 
-  
+import subtract from "./math_functions.js";
 // Only change code above this line
 
 subtract(7,4);
