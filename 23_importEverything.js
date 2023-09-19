bbd5e441f04c0cdc5384615a9e9b47deea908353
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 10:47:22 +0300

    Challenge - solution

--- 23_importEverything.js
+++ 23_importEverything.js
@@ -10,7 +10,7 @@ myMathModule.subtract(5, 3);
 that is in the same directory as the current file.  Use the import * as syntax 
 to import everything from the file into an object called stringFunctions. */
 
-
+import * as stringFunctions from './string_functions.js';
 // Only change code above this line
 
 stringFunctions.uppercaseString("hello");
