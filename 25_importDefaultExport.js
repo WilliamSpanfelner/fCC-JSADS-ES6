4ec17e36420ef4dafa1e2a20b671321658b34bea
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 11:36:16 +0300

    Challenge - start

--- 25_importDefaultExport.js
+++ 25_importDefaultExport.js
@@ -4,3 +4,12 @@ export default - no curly braces are used and the
 imported value is simply a variable name*/
 
 import add from "./math_functions.js";
+
+/* In the following code, import the default export from the 
+math_functions.js file, found in the same directory as this file. 
+Give the import the name subtract. */
+
+  
+// Only change code above this line
+
+subtract(7,4);
