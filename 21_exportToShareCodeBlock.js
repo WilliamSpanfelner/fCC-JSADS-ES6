ad25a8a6c9225ec67b4fc5fee4307e0722d53a19
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 10:06:49 +0300

    Challenge - start

--- 21_exportToShareCodeBlock.js
+++ 21_exportToShareCodeBlock.js
@@ -15,3 +15,13 @@ export { add };
 
 // Export multiple things:
 export { add, subtract };
+
+/* There are two string-related functions in the editor. 
+Export both of them using the method of your choice. */
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  
+  const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
