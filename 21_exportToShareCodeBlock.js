71e553d2726efab0f513a19aac3b1143a2fe1b9b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 10:02:55 +0300

    Export re-write

--- 21_exportToShareCodeBlock.js
+++ 21_exportToShareCodeBlock.js
@@ -1,6 +1,14 @@
 // Use export to Share a Code Block
 /* If there were a file called math_functions.js 
 containing a function called add, it could be exported like this: */
-export const add = (x, y) => {
+
+// export const add = (x, y) => {
+//     return x + y;
+// }
+
+// The above could also be written:
+const add = (x, y) => {
     return x + y;
 }
+
+export { add };
