6c9d1f32bcf0d06a948a23373036f325b853ad01
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 12:11:23 +0300

    Add example code for then method

--- 28_handleFulfilledPromiseWThen.js
+++ 28_handleFulfilledPromiseWThen.js
@@ -1,1 +1,8 @@
 // Handle a Fulfilled Promise with then
+// Immediately after a promise is fulfilled the then method is executed.
+
+myPromise.then(result => {
+
+});
+
+// where result derives from the argument given to the resolve method.
