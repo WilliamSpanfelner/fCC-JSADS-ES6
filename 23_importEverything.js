37b0a718bee48ce710da21f381b887cc038a1a17
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 10:37:26 +0300

    Add import everything syntax

--- 23_importEverything.js
+++ 23_importEverything.js
@@ -1,1 +1,3 @@
 // Use * to Import Everything from a File
+// import * as allows the entire file to be imported
+import * as myMathModule from './math_functions.js';  // myMathModule is just a name and can be anything
