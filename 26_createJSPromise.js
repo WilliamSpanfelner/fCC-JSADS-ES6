498c935b8821271ef0e98646b2ba2a216abcb376
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-09-19 11:46:47 +0300

    Add promise syntax

--- 26_createJSPromise.js
+++ 26_createJSPromise.js
@@ -1,1 +1,9 @@
 // Create a JavaScript Promise
+/* A promise is used to do something, usually asynchronously, 
+when a task completes.  The new keyword is required when using
+promise as it is a constructor function. The constructor takes 
+two parameters resolve and reject. The syntax is as follows:
+*/
+const myPromise = new Promise((resolve, reject) => {
+
+});
